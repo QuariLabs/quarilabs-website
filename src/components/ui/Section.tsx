95077c2624b28927eb91b176aee3754501456059
Jonath-z <jonathanzihindula95@gmail.com>
2024-07-20 22:55:56 +0200

    feat(ui): adjust padding for large screens

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -5,7 +5,7 @@ const Section = (props: ComponentProps<"div">) => {
   return (
     <div
       {...rest}
-      className={`lg:max-w-screen-[1512px] max-w-full max-sm:px-5 max-sm:py-7 px-[5rem] w-full mx-auto py-20 max-lg:px-[3rem] xl:px-[5rem] 2xl:px-[20rem] ${className}`}
+      className={`lg:max-w-screen-[1512px] max-w-full max-sm:px-5 max-sm:py-7 px-[5rem] w-full mx-auto py-20 max-lg:px-[3rem] xl:px-[5rem] 2xl:px-[10rem] min-[1700px]:px-[20rem] ${className}`}
     >
       {children}
     </div>
